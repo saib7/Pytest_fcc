290501ba37e333059dd41eb19b9d587757674638
Saib Ahmed <saibahmed7@gmail.com>
2025-01-15 22:09:18 +0600

    class based tests added

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -9,3 +9,28 @@ def test_add():
     assert my_functions.add(-1, -1) == -2
     assert my_functions.add(1, -1) == 0
 
+
+def test_add_string():
+    assert my_functions.add("a", "b") == "ab"
+    with pytest.raises(TypeError):
+        my_functions.add("a", 1)
+    with pytest.raises(TypeError):
+        my_functions.add(1, "b")
+
+
+
+def test_divide():
+    assert my_functions.divide(1, 2) == 0.5
+    with pytest.raises(ZeroDivisionError):
+        my_functions.divide(0, 0)
+    assert my_functions.divide(-1, 1) == -1
+    assert my_functions.divide(-1, -1) == 1
+    assert my_functions.divide(1, -1) == -1
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        my_functions.divide(1, 0)
+    with pytest.raises(ZeroDivisionError):
+        my_functions.divide(-1, 0)
+
+
