04caca4365a8d8a6d1c235e7991b9071ea33a0d9
Saib Ahmed <saibahmed7@gmail.com>
2025-01-16 21:40:21 +0600

    conftest and pytest fixture added

--- source/shapes.py
+++ source/shapes.py
@@ -19,4 +19,19 @@ class Circle(Shape):
         return 2 * math.pi * self.radius
 
 
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def __eq__(self, other):
+        if not isinstance(other, Rectangle):
+            return False
+        return self.width == other.width and self.height == other.height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
 
